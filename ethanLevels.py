4e47d237e96194d99e55e266c556cf4d713cb944
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-08-15 11:03:27 -0700

    Update levels with new platforms and colors

--- ethanLevels.py
+++ ethanLevels.py
@@ -1,24 +1,40 @@
 from ethanPlatform import *
 
-# xpos, ypos, xwidth, yheight, color
-
 
 def level1():
-    return [
-        CustomPlatform(0,   600, 1280, 20, "white"),   # ground
-        CustomPlatform(500, 250, 150,  20, "white"),
-        CustomPlatform(400, 450, 150,  20, "white"),
-        CustomPlatform(700, 150, 150,  20, "white"),
-        CustomPlatform(250, 350, 150,  20, "white"),
-        EscapeDoor(760, 90, 30, 60),
+    return[
+        CustomPlatform(0, 600, 1280, 20, "red"),
+        CustomPlatform ( 450, 450, 200 ,20, "red"),
+        CustomPlatform ( 450, 325,200,20, "red" ),
     ]
+
 
 
 def level2():
-    return [
-        CustomPlatform(0, 600, 1280, 20, "white"),   # ground
-        EscapeDoor(1200, 540, 30, 60),
+    return[
+        CustomPlatform(0, 600, 1280, 20, "red"), 
+        CustomPlatform ( 800, 400, 200 ,20, "red"),
+        CustomPlatform ( 785, 303,200,20, "red" ),
+        CustomPlatform(785, 203, 10, 100, "red" ),
+        CustomPlatform(785, 33, 10, 100, "red" ),
+        CustomPlatform(50, 530, 150, 20, "red" ),
+        CustomPlatform(195, 400, 10, 150, "red" ),
+        CustomPlatform(50, 400, 150, 20, "red" ),
+        CustomPlatform(300, 400, 35, 20, "red" ),
+        CustomPlatform(600, 400, 35, 20, "red" ),
+        EscapeDoor(300, 150, 100, 20, 0,500 ),
+        CustomPlatform(300, 170, 100, 20, "red" ),
+
     ]
 
 
-levels = [level1, level2]
+
+def level3():
+    return[
+        CustomPlatform(0, 600, 1280, 20, "red"), 
+        CustomPlatform ( 250, 450, 200 ,20, "red"),
+        CustomPlatform ( 250, 325,200,20, "red" ),
+    ]
+
+
+levels = [level1, level2, level3]
