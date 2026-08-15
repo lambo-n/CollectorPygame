caef75cfa06f7eda109a820de09b71670a708207
lambo-n <86932850+lambo-n@users.noreply.github.com>
2026-08-15 11:20:20 -0700

    Refactor platform update methods and EscapeDoor init
    Refactor update methods to use player_rect instead of playerHitbox and add spawn coordinates to EscapeDoor.

--- ethanPlatform.py
+++ ethanPlatform.py
@@ -8,8 +8,9 @@ class CustomPlatform:
         self.height = height
         self.color = color
         self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
-        
-      
+
+
+
     @property
     def top(self): return self.rect.top
     @property
@@ -19,16 +20,24 @@ class CustomPlatform:
     @property
     def right(self): return self.rect.right
 
-    def update(self, screen, playerHitbox=None):
+    def update(self,screen, player_rect=None):
         pygame.draw.rect(screen, self.color, self.rect)
+        return None
+
+
 
 
 class EscapeDoor(CustomPlatform):
-    def __init__(self, posx, posy, width, height):
+    def __init__(self, posx, posy, width, height, spawnx, spawny):
         super().__init__(posx, posy, width, height, "green")
+        self.spawnx = spawnx
+        self.spawny = spawny 
 
-    def update(self, screen, playerHitbox=None):
+
+    def update(self, screen, player_rect):
         super().update(screen)
 
-        if playerHitbox is not None and playerHitbox.colliderect(self.rect):
+        if player_rect is not None and player_rect.colliderect(self.rect):
             return "escape"
+
+
